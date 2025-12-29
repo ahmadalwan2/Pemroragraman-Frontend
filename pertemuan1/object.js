a060aedc20c66e2b4da75f6c4c38be3ebbf2232f
ahmadalwan <madajadeh2023@gmail.com>
2025-12-29 11:08:04 +0700

    Menambahkan file object.js

---
+++ pertemuan1/object.js
@@ -0,0 +1,15 @@
+// === Membuat Object di JavaScript yang sederhana === //
+
+// - Menggunakkan tanda {}
+// - Menggunakkan new Object {}
+
+const user1 = {
+    name: "Ucup",
+    age: 18,
+    address: "Depok",
+};
+
+const user2 = new Object();
+user2.name = "Udin";
+user2.age = 17;
+user2.address = "Bogor";
