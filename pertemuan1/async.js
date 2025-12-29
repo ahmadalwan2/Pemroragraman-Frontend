ceeea5462cf0cfd3b58271d157bb4e0e73cc2925
ahmadalwan <madajadeh2023@gmail.com>
2025-12-29 13:44:29 +0700

    Membuat file async.js dan mengisi data dari file tsbt

---
+++ pertemuan1/async.js
@@ -0,0 +1,17 @@
+// === Simulasi Asyncronous yang sederhana didalam JavaScript === //
+
+function rebusAir() {
+    console.log("Rebus air");
+}
+
+function masakMie() {
+    console.log("Masak mie");
+}
+
+function makanMie() {
+    console.log("Makan mie");
+}
+
+rebusAir();
+masakMie();
+makanMie();
